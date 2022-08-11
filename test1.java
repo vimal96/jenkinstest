fa1ef533ceec23452d5a6d9cd83657509837fb7c
vimal96 <vimalvenugopal25@gmail.com>
2022-08-11 03:18:08 -0400

    modified the java program

--- test1.java
+++ test1.java
@@ -1,10 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
         
-        int num1 = 7, num2 = 8, sum;
-        sum = num1 + num2;
 
-        System.out.println("Sum of two numbers: " + sum);
+
+        System.out.println("hi");
     }
 }
 
